15155c9d7e01b27a999c561988290768cd0f85e4
Kenny Johnston <kencjohnston@gmail.com>
2016-02-26 11:46:58 -0600

    Added Support for Targetting a Specific Project/Module
    Adjusted to add an additional argument to filter on a single project

--- report.py
+++ report.py
@@ -99,49 +99,50 @@ def debug(d):
     print(json.dumps(d, indent=4, sort_keys=True))
 
 
-def summarize(events, gerrit_user_ids):
+def summarize(events, gerrit_user_ids, project):
     for event in events:
         if not gerrit_user_ids or event['gerrit_id'] in gerrit_user_ids:
-            # When?
-            message = ['[{date_str}]']
+            if not project or event['module'] == project:
+                # When?
+                message = ['[{date_str}]']
 
-            # Who?
-            message.append('{gerrit_id} ({company_name})')
+                # Who?
+                message.append('{gerrit_id} ({company_name})')
 
-            # What?
-            if event['type'] in ('Workflow', 'Self-Workflow'):
-                if event['value'] == -1:
-                    message.append('WIP\'d')
-                elif event['value'] == 1:
-                    message.append('approved')
+                # What?
+                if event['type'] in ('Workflow', 'Self-Workflow'):
+                    if event['value'] == -1:
+                        message.append('WIP\'d')
+                    elif event['value'] == 1:
+                        message.append('approved')
+                    else:
+                        debug(event)
+                        quit()
+                elif event['type'] in ('Code-Review', 'Self-Code-Review'):
+                    if event['value'] == 2:
+                        message.append('+2\'d')
+                    elif event['value'] == 1:
+                        message.append('+1\'d')
+                    elif event['value'] == -1:
+                        message.append('-1\'d')
+                    elif event['value'] == -2:
+                        message.append('blocked')
+                    else:
+                        debug(event)
+                        quit()
+                elif event['type'] in ('Abandon', 'Self-Abandon'):
+                    message.append('abandoned')
                 else:
                     debug(event)
                     quit()
-            elif event['type'] in ('Code-Review', 'Self-Code-Review'):
-                if event['value'] == 2:
-                    message.append('+2\'d')
-                elif event['value'] == 1:
-                    message.append('+1\'d')
-                elif event['value'] == -1:
-                    message.append('-1\'d')
-                elif event['value'] == -2:
-                    message.append('blocked')
-                else:
-                    debug(event)
-                    quit()
-            elif event['type'] in ('Abandon', 'Self-Abandon'):
-                message.append('abandoned')
-            else:
-                debug(event)
-                quit()
 
-            message.append('"{parent_subject}" ({parent_number})')
+                message.append('"{parent_subject}" ({parent_number})')
 
-            # Where?
-            message.append('in {module}.')
+                # Where?
+                message.append('in {module}.')
 
-            message = u' '.join(message)
-            print(message.format(**event))
+                message = u' '.join(message)
+                print(message.format(**event))
 
 
 def activity(start_date, end_date):
@@ -186,7 +187,7 @@ def main(args):
         })
     )
 
-    summarize(activity(start_date, end_date), args.gerrit_user_ids)
+    summarize(activity(start_date, end_date), args.gerrit_user_ids, args.project)
 
 
 if __name__ == '__main__':
@@ -199,6 +200,9 @@ if __name__ == '__main__':
     parser.add_argument(
         '--reporting-period', type=int, default=7,
         help='Period (in days) to report on.')
+    parser.add_argument(
+        '--project',
+        help='Project to report on.')
     parser.add_argument(
         'gerrit_user_ids', nargs=argparse.REMAINDER,
         help='Specify gerrit users to filter on.')
